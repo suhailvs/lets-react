f6bf8e326b59e11b2c54c9015f9c2e73c962438f
suhailvs <suhailvs@gmail.com>
2025-07-04 18:30:44 +0530

    added send money

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,13 +1,19 @@
 import { useParams } from 'react-router';
 import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 import API from '../utils/api';
 
 export default function User() {
   const params = useParams();
+  const navigate = useNavigate();
   const userid = params['id'];
 
   const [user, setUser] = useState({});
+  const [amount, setAmount] = useState("");
   const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
   useEffect(() => {
     getUser();
   }, []);
@@ -21,15 +27,57 @@ export default function User() {
       setLoading(false);
     }
   };
+  const handleSendMoney = async () => {
+      setError("");  // Clear previous errors
+      setLoading(true);
+      try {
+        const response = await API.post('/transactions/',{
+          user: userid,
+          amount: amount,
+          message: message
+        });
+        navigate("/");
+        // setModalVisible(false); 
+        // router.replace({ pathname: 'screens/sendmoney/success',params: {name:first_name, amount:amount } });
+        // setOffering(response.data);
+      } catch (error) {
+        if (error.response) {
+          setError(JSON.stringify(error.response.data)|| "Invalid credentials");
+        } else if (error.request) {
+          setError("Network error. Please try again.");
+        } else {
+          setError("Something went wrong. Please try again.");
+        }      
+      } finally {
+        setLoading(false);      
+      }    
+    };
   return (
   <div>
-    User: {userid}
     {loading==true ? (<div>loading...</div>): (
-      <div>
-        <img className="img-thumbnail mr-3" src={user.image}/>
-        £ {user.balance ?? 0}
-      
+    <div className="card" style={{ width: '18rem'}}>
+      <img src={user.image} className="card-img-top" alt={user.first_name} />
+      <div className="card-body">
+        <h5 className="card-title">{user.first_name} ({user.username})</h5>
+        <p className="card-text">balance: £ {user.balance ?? 0}</p>
+        <input
+          className="form-control mb-4"
+          placeholder="Enter message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <input
+          type="number"
+          className="form-control form-control-lg text-center"
+          placeholder="Enter amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button className="btn btn-primary btn-lg" onClick={handleSendMoney}>Proceed</button>
       </div>
+      {error}
+    </div>
+    
     )}      
   </div>
   )
